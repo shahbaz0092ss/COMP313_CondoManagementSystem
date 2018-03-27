4cbfcc8ed8db3c556357a8c844cb1a764fdc81a1
AlnoorS <alnoors@github.com>
2018-03-27 18:24:28 -0400

    Model created: Maintenance.cs

---
+++ ShahbazWebsite_MVCPlatform/Models/Maintenance.cs
@@ -0,0 +1,36 @@
+
+using System;
+
+using System.Collections.Generic;
+
+
+
+namespace ShahbazWebsite_MVCPlatform.Models
+
+{
+
+    public partial class Maintenance
+
+    {
+
+        public int MaintenanceId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Date { get; set; }
+
+        public string MaintenanceDetail { get; set; }
+
+        public string Status { get; set; }
+
+        public int DollarAmount { get; set; }
+
+        public string Note { get; set; }
+
+        public User User { get; set; }
+
+    }
+
+}
