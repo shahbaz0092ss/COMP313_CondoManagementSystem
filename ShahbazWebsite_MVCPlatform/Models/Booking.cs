66315f57381f7ebd98794a62d37466f818700452
AlnoorS <alnoors@github.com>
2018-03-27 18:17:47 -0400

    Model created: Booking.cs

---
+++ ShahbazWebsite_MVCPlatform/Models/Booking.cs
@@ -0,0 +1,35 @@
+using System;
+
+using System.Collections.Generic;
+
+
+
+namespace ShahbazWebsite_MVCPlatform.Models
+
+{
+
+    public partial class Booking
+
+    {
+
+        public int BookingId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Date { get; set; }
+
+        public string TimeFrom { get; set; }
+
+        public string TimeTo { get; set; }
+
+        public string BookingDetail { get; set; }
+
+        public string Status { get; set; }
+
+        public User User { get; set; }
+
+    }
+
+}
