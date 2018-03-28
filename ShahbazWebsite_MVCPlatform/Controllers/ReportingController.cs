12cc206bb51b0e7bce401b43e0494ba43819775c
AlnoorS <alnoors@github.com>
2018-03-28 06:24:31 -0400

    Reports 1) Requests and 2) Maintenance data can be generated by Employee.

--- ShahbazWebsite_MVCPlatform/Controllers/ReportingController.cs
+++ ShahbazWebsite_MVCPlatform/Controllers/ReportingController.cs
@@ -3,54 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShahbazWebsite_MVCPlatform.Models;
 
 namespace ShahbazWebsite_MVCPlatform.Controllers
+
+
 {
     public class ReportingController : Controller
     {
 
-        // Nothing yet
+
+        private readonly comp313MVCDatabaseContext _context;
+
+        public ReportingController(comp313MVCDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // Default index - Nothing yet
         public IActionResult Reporting()
         {
             return View();
         }
 
-        public IActionResult Reports_DailyRequestsAndBookings()
+          // GET: Bookings and Requests report - Show all data
+        public async Task<IActionResult> Reports_DailyRequestsAndBookings()
         {
 
+            var comp313MVCDatabaseContext = _context.Booking.Include(b => b.User);
+            return View(await comp313MVCDatabaseContext.ToListAsync());
 
-            if (TempData.ContainsKey(key: "EmployeeLogin"))
-            {
-
-
-                return View();
-
-            }
-            else
-            {
-                // Take to Login page
-                // Login action, in Login controller
-                return RedirectToAction("Login", "Login");
-            }
 
         }
 
-        public IActionResult Reports_Maintenance()
+           // GET: Maintenances report - Show all data
+        public async Task<IActionResult> Reports_Maintenance()
         {
-            if (TempData.ContainsKey(key: "EmployeeLogin"))
-            {
-
-
-                return View();
-
-            }
-            else
-            {
-                // Take to Login page
-                // Login action, in Login controller
-                return RedirectToAction("Login", "Login");
-            }
+            var comp313MVCDatabaseContext = _context.Maintenance.Include(m => m.User);
+            return View(await comp313MVCDatabaseContext.ToListAsync());
         }
+
 
         public IActionResult Reports_TenantSearchDetails()
         {
